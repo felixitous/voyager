9b3dc982c3c2417cee96611eabc85e6ca9f59b71
Felix Liu <felixliu@berkeley.edu>
2015-08-12 00:25:12 -0700

    about page completion, footer added

--- voyager/static/js/tween.js
+++ voyager/static/js/tween.js
@@ -1,12 +1,12 @@
 function companyAnimation() {
     $(".company-overlay").mouseenter(function() {
-        TweenMax.to(this, 0.5, {
+        TweenMax.to(this, 0.3, {
             opacity: "0"
         });
     });
     $(".company-overlay").mouseleave(function() {
-        TweenMax.to(this, 0.5, {
-            opacity: "0.3"
+        TweenMax.to(this, 0.3, {
+            opacity: "0.2"
         });
     });
 };
@@ -100,20 +100,20 @@ function navbarLaunch() {
 }
 
 
-var lastScrollTop = 0;
-$(window).scroll(function(event) {
-    var st = $(this).scrollTop();
-    if (st > lastScrollTop) {
-        TweenMax.to($(".navbar"), 0.4, {
-            opacity: 0
-        });
-    } else {
-        TweenMax.to($(".navbar"), 0.4, {
-            opacity: 1
-        });
-    }
-    lastScrollTop = st;
-});
+// var lastScrollTop = 0;
+// $(window).scroll(function(event) {
+//     var st = $(this).scrollTop();
+//     if (st > lastScrollTop) {
+//         TweenMax.to($(".navbar"), 0.4, {
+//             opacity: 0
+//         });
+//     } else {
+//         TweenMax.to($(".navbar"), 0.4, {
+//             opacity: 1
+//         });
+//     }
+//     lastScrollTop = st;
+// });
 
 // console.log(window.innerHeight);
 $(".grid-picture").css("width", function() {
