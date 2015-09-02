6dfaa37709c684db3dca41800f708a232ffab111
Felix Liu <felixliu@berkeley.edu>
2015-09-02 11:43:27 -0700

    better text box sizing

--- voyager/static/js/tween.js
+++ voyager/static/js/tween.js
@@ -298,7 +298,13 @@ $(".company").css("height", function() {
 });
 
 $(".services-box").css("width", function() {
-    return $(".services-box-container").width() / 3 - 50;
+    target_width = $(".services-box-container").width() / 3 - 50;
+    if (target_width < 282) {
+        return 288;
+    } else {
+        return target_width;
+    };
+    // return $(".services-box-container").width() / 3 - 50;
 });
 
 $("#apply-form").submit(function(event) {
